3cb8fbd7af9565dcbb6354ec3b3a83dcb8c55ead
Kazuki Shimizu <kazubu@jtime.net>
2024-08-30 03:00:18 +0900

    improve key configuration

--- keyboards/ymdk/ymd40/air40/keymaps/via/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/via/keymap.c
@@ -28,25 +28,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT,  MO(2),   KC_RALT, KC_APP,  KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  MO(2),   KC_RALT, KC_APP,  KC_RCTL
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
-        KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-        KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,KC_TRNS,KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+        _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
-        KC_TRNS, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_MINS), S(KC_EQL), S(KC_LBRC),S(KC_RBRC),S(KC_BSLS), S(KC_GRV),
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_MINS), S(KC_EQL), S(KC_LBRC),S(KC_RBRC),S(KC_BSLS), S(KC_GRV),
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_ADJUST] = LAYOUT_ortho_4x12(
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, RGB_HUI, KC_BRIU, KC_VOLU, KC_LNG2, RGB_VAI, RGB_SAI, KC_LNG1, _______, _______, _______, KC_F12,
-        _______, RGB_HUD, KC_BRID, KC_VOLD, KC_MUTE, RGB_VAD, RGB_SAD, _______, _______, _______, KC_SCRL, KC_PSCR,
-        QK_BOOT, RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, AG_NORM, AG_SWAP, DB_TOGG
+        _______, RGB_HUI, KC_BRIU, KC_VOLU, KC_LNG2, RGB_VAI, RGB_SAI, KC_LNG1, RGB_TOG, XXXXXXX, XXXXXXX, KC_F12,
+        _______, RGB_HUD, KC_BRID, KC_VOLD, KC_MUTE, RGB_VAD, RGB_SAD, XXXXXXX, RGB_MOD, XXXXXXX, KC_SCRL, KC_PSCR,
+        QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DB_TOGG
     )
 };
 
