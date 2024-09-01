1657f2697ecd566e162eede1db8e84b40e586e3b
Kazuki Shimizu <kazubu@jtime.net>
2024-09-02 01:03:40 +0900

    enable dynamic tapping and HOLD_ON_OTHER_KEY_PRESS

--- keyboards/ymdk/ymd40/air40/keymaps/via/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/via/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  XXXXXXX, XXXXXXX, KC_ENT,  MO(2),   KC_RALT, KC_APP,  KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, LT(1,KC_LNG2),KC_SPC,XXXXXXX,XXXXXXX,KC_ENT, LT(2,KC_LNG1),KC_RALT,KC_APP,KC_RCTL
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_ADJUST] = LAYOUT_ortho_4x12(
         KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, RGB_HUI, KC_BRIU, KC_VOLU, KC_LNG2, RGB_VAI, RGB_SAI, KC_LNG1, RGB_TOG, XXXXXXX, XXXXXXX, KC_F12,
-        _______, RGB_HUD, KC_BRID, KC_VOLD, KC_MUTE, RGB_VAD, RGB_SAD, XXXXXXX, RGB_MOD, XXXXXXX, KC_SCRL, KC_PSCR,
+        _______, RGB_HUI, KC_BRIU, KC_VOLU, KC_LNG2, RGB_VAI, RGB_SAI, KC_LNG1, RGB_TOG, DT_UP  , DT_PRNT, KC_F12,
+        _______, RGB_HUD, KC_BRID, KC_VOLD, KC_MUTE, RGB_VAD, RGB_SAD, XXXXXXX, RGB_MOD, DT_DOWN, KC_SCRL, KC_PSCR,
         QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DB_TOGG
     )
 };
